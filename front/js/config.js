46ff68c7f3cba788536f25f747104dc2ab459f45
Winters67 <emmanuel.vbg@gmail.com>
2022-04-09 15:31:28 +0200

    correction d'index

--- front/js/config.js
+++ front/js/config.js
@@ -1,17 +1,10 @@
-
 /**Paramètre des api
- * "getAllProduct" : "/", ==> Retourne un tableau de tous les éléments
-    "getProduct" : "/", ==> Renvoie l'élément correspondant à {product-id}, identifiant d'un produit
-    "newsOrder" : "/order", ==> retourne l'objet contact
- * 
- * 
+ *
+ *
  */
 /** */
 const config = {
-  "api" : {
-    "baseUrl": "http://localhost:3000/api/products",
-    "getAllProduct" : "/",
-    "getProduct" : "/",
-    "newsOrder" : "/order",
-  }
-}
+  api: {
+    baseUrl: "http://localhost:3000/api/products",
+  },
+};
--- front/js/index.js
+++ front/js/index.js
@@ -1,18 +1,24 @@
-// Récupération de la liste des produits et de leurs détails, depuis l'API.
+/*************************************************************
+ Récupération de la liste des produits et de leurs détails,
+ adresse de l'api dans le fichier config.js
+ ************************************************************/
+
 async function getItems() {
-    try {
-        const res = await fetch("http://localhost:3000/api/products");
-        const items = await res.json();
-        return items;
-    }
-    catch (error) {
-        console.log("Erreur: " + error);
-    };
+  try {
+    res = await fetch(config.api.baseUrl);
+    items = await res.json();
+    return items;
+  } catch (error) {
+    console.log("Erreur: " + error);
+  }
 }
 
-// Modification du DOM pour faire apparaitre les produits.
+/************************************************************
+ * Modification du DOM pour faire apparaitre les produits ***
+ ************************************************************/
+
 function showItem(item) {
-    document.querySelector("#items").innerHTML += `
+  document.querySelector("#items").innerHTML += `
         <a href="./product.html?id=${item._id}">
             <article>
                 <img src="${item.imageUrl}" alt="${item.altTxt}">
@@ -22,10 +28,13 @@ function showItem(item) {
         </a>`;
 }
 
-// Rendu dynamique de la liste des produits.
+/************************************************************
+ ***** Boucle affichage des produits de la page accueil ******
+ ************************************************************/
+
 (async function renderProducts() {
-    const items = await getItems();
-    items.forEach(item => {
-        showItem(item)
-    });
-})()
+  items = await getItems();
+  items.forEach((item) => {
+    showItem(item);
+  });
+})();
